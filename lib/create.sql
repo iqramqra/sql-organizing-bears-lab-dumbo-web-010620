256e5c285b626b85bf2778cf665454b679659450
Iqra Masroor <masrooriqra@gmail.com>
2020-01-16 16:25:25 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -6,15 +6,4 @@ CREATE TABLE bears (
   color TEXT,
   temperament TEXT,
   alive BOOLEAN
-  
 );
-
--- CREATE TABLE bears (
-    id INTEGER PRIMARY KEY,
-    name TEXT,
-    age INTEGER,
-    gender TEXT,
-    color TEXT,
-    temperament TEXT,
-    alive BOOLEAN
--- ); 
