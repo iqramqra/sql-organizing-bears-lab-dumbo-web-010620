5ec7cdd6a99ac6c96998868f917534fc3f4366a4
Iqra Masroor <masrooriqra@gmail.com>
2020-01-16 16:36:21 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES 
-(1, "Mr.Chocolate", 2, "female", "blue", "crybaby", true);
-(2, "Rowdy", 10, "male", "red", "annoying", false);
+(1, "Mr.Chocolate", 2, "female", "blue", "crybaby", 1);
+(2, "Rowdy", 10, "male", "red", "annoying", 0);
 (3, "Tabitha", 23, "female", "brown", "nice", true);
 (4, "Sergeant Brown", 30, "female", "brown", "strict", true);
 (5, "Melissa", 15, "male", "yellow", "happy", true);
