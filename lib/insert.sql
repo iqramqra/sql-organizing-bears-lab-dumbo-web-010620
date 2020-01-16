0507dccaa13fc8bce39a13a487987a3f7c277598
Iqra Masroor <masrooriqra@gmail.com>
2020-01-16 16:33:26 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,5 +4,5 @@ INSERT INTO cats (id, name, age, gender, color, temperament, alive) VALUES
 (3, "Tabitha", 23, "female", "brown", "nice", true);
 (4, "Sergeant Brown", 30, "female", "brown", "strict", true);
 (5, "Melissa", 15, "male", "yellow", "happy", true);
-(6, "Grinch", 15, "female", "grey", "calm", false);
-(7, "Mr.Chocolate", 2, "female", "blue", "crybaby", true);
+(6, "Grinch", 50, "female", "grey", "calm", false);
+(7, "Wendy", 12, "male", "grey", "calm", true);
